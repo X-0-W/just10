61b9d2f4c259cde3f614d91c35ff0fba785cda3b
Xander Wellingham <xwellingham@gmail.com>
2022-06-06 18:18:46 +1000

    just10 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :listings, except: [:destroy] do
+    resources :transactions, only: [:new, :create]
+  end
+  resources :chatrooms, only: [:show]
+  resources :transactions, only: [:index] do
+    member do
+      patch :accept
+      patch :reject
+    end
+    resources :reviews, only: [:new, :create]
+  end
 end
+
+# patch '/transactions/:id/accept', to: 'transactions#accept', as :accept
+# /transactions/:id/accept
+# /transactions/:id/reject
