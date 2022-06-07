5ed41f48bbefd4aa601ef3881381715a4708a8c3
Ethan Li <ethanli588@gmail.com>
2022-06-07 23:59:31 +1000

    backend of show, new, create, edit and update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Listing.destroy_all
+User.destroy_all
+
+new_user1 = User.create!(email: "xander@live.com", password: "testing")
+new_user2 = User.create!(email: "reiko@live.com", password: "testing")
+new_user3 = User.create!(email: "callum@live.com", password: "testing")
+new_user4 = User.create!(email: "ethan@live.com", password: "testing")
+
+Listing.create!(title: "toy", collection_instruction: "pick up at front door", user: new_user1)
+Listing.create!(title: "pen", collection_instruction: "pick up at frontyard", user: new_user1)
+Listing.create!(title: "flower", collection_instruction: "knock the door before pick up", user: new_user1)
+Listing.create!(title: "book", collection_instruction: "knock reiko's door", user: new_user2)
