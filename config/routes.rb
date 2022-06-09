367e44430601fafc48933abf45cc36f6a443a0f4
Xander Wellingham <xwellingham@gmail.com>
2022-06-09 17:02:27 +1000

    routes'

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,22 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/dashboard', to: 'pages#dashboard', as: 'dashboard'
   resources :listings, except: [:destroy] do
-    resources :orders, only: [:new, :create]
+    resources :orders, only: [:index, :new, :create]
   end
   resources :chatrooms, only: [:show]
   resources :orders, only: [:index] do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :orders, only: [:show] do
     member do
       patch :accept
       patch :reject
     end
-    resources :reviews, only: [:new, :create]
   end
 end
+
+# row 9-15 to below row 6
+# add resources: orders, only: [:show] - not nested. Benefit vs nesting?
 
 # patch '/orders/:id/accept', to: 'orders#accept', as :accept
 # /orders/:id/accept
