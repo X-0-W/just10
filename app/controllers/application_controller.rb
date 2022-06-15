d65e1616ae80dacdc424fa1a18c0a78fa918c28a
Xander Wellingham <xwellingham@gmail.com>
2022-06-15 14:42:44 +1000

    footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :user_notification_count
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -8,6 +9,10 @@ class ApplicationController < ActionController::Base
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address])
+  end
+
+  def user_notification_count
+
   end
 
 end
